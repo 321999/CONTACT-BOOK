067d147b202853fcc2fa29e3a0b4ebf258898dba
321999 <kishorejha321999@gmail.com>
2024-01-25 21:11:33 +0530

    contact  page is created

--- contactbook/contacts/views.py
+++ contactbook/contacts/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 # logic for adding the contact details 
-def add_contact(self):
+def add_contact(request):
     print("adding the details ")
-    return HttpResponse("ADD NEW CONTACT ")
+    return render(request,"addcontact.html")
